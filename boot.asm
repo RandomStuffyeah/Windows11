2e00dbb3d8d07ed2a52892d44dc6db4d7576679d
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2019-07-10 21:20:34 -0400

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,20 +1,6 @@
 // Windows 11
-findcpu Intel X96
-or else
-print "UNSUPPORTED CPU"
-then
-include "c:\windows\system32\bootsound.asm"
-iffound
-include "c:\windows\asm\sound.asm"
-iffound
 include "c:\windows\winvercode.asm"
 iffound
 start "c:\windows\kernel.exe"
 start "c:\windows\system.exe"
 start "c:\autoexec.bat"
-
-// Windows 11
-
-If version = "COREOS"
-
-start "c:\windows\system32\command.com"
